5cdb1debf42e4b9d3956d3183447866525ac28b8
Robert Anulewicz <44050586+ranulewicz@users.noreply.github.com>
2018-10-11 17:35:23 +0200

    Add files via upload

---
+++ kot.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	puts("Kot");
+	putchar('g');
+	putchar('\n');
+	putchar('\t');
+	puts("grzecznie spi");
+	putchar('\r');
+	putchar('\\');
+	/* dupa */
+
+	return 0;
+}
