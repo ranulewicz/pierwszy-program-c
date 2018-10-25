2553bc8046f28d0b7187278587a11ed8bf760b36
Robert Anulewicz <44050586+ranulewicz@users.noreply.github.com>
2018-10-25 16:47:50 +0200

    Add files via upload

---
+++ definicja_testow_w_c.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define TEST
+int dodaj(int a, int b){
+    return 9;
+}
+
+
+
+#ifdef TEST
+
+void nd(int licznik) { printf("Test nr %i nie działa\n", licznik); }
+
+// tutaj należy umieszczać kolejne testy
+
+void test1_dodaj(int licznik) {
+    if(dodaj(3, 6) !=9) nd(licznik);
+}
+void test2_dodaj(int licznik) {          //    TUTAJ jest kolejne działanie i to nie przejdzie bo nie ma na gorze return 11
+    if(dodaj(5, 6) !=11) nd(licznik);
+}
+void run_test_pool() {
+    int licznik = 0;
+// tutaj wywołujemy testy
+
+    test1_dodaj(++licznik);
+    test2_dodaj(++licznik);
+}
+
+#endif
+
+
+int main() {
+
+#ifndef TEST
+    // uruchom program
+
+
+#else
+    // uruchom testy
+    run_test_pool();
+
+#endif
+
+    return 0;
+}
